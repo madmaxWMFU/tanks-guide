f445f1066ff5772b6119e2e88623be569a586a1d
madmaxWMFU <yasmaks@gmail.com>
2021-04-30 02:07:01 +0300

    add func

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-import { getUrl } from './utils';
+import { getUrl, getRomeNumber, addActiveFilter, filterByTier } from './utils';
 
 if (module.hot) {
   module.hot.accept();
 }
 
 window.dataStore = {
+  activeFilters: {},
   status: {
     error: null,
     process: null,
@@ -29,21 +30,22 @@ window.renderApp = renderApp;
 window.loadData = loadData;
 window.performSearch = performSearch;
 window.searchByFilter = searchByFilter;
+window.searchById = searchById;
 
 performSearch(window.dataStore.init.cache, window.dataStore.init.path, window.dataStore.init.param);
 renderApp();
 
 function renderApp() {
   document.querySelector('#app-root').innerHTML = `
-        ${App()}
-    `;
+      ${App()}
+  `;
 }
 
-function loadData(path, param) {
+async function loadData(path, param) {
   const url = getUrl(path, param);
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => data);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 function performSearch(cache, path, param) {
@@ -64,28 +66,77 @@ function performSearch(cache, path, param) {
 }
 
 function searchByFilter(vehicle) {
-  const cacheType = vehicle.type;
-  const [key, value] = vehicle.value.split('-');
+  const cacheType = vehicle.dataset.type;
+  const [key, value] = vehicle.dataset.value.split('_');
   window.dataStore.filters[key] = value;
   window.performSearch(cacheType, 'vehicles', window.dataStore.filters);
+  addActiveFilter(vehicle);
 }
 
-function searchById(vehicle) {
-  const cacheType = vehicle.type;
-  const [key, value] = vehicle.value.split('-');
-  window.dataStore.filters[key] = value;
-  window.performSearch(cacheType, 'vehicleprofile', window.dataStore.filters);
-}
-
-function getVehicle() {
-  const data = window.dataStore.cache.vehicleData;
-  const data1 = window.dataStore.cache.searchData;
-  if (data) {
-    const idVehicle = Object.values(data)[0].tank_id;
-    return `
-      <div> 
-        <p>${data1[idVehicle].description}</p>
-        <p>Weight: <b>${Object.values(data)[0].weight}</b></p>
+function searchById(id) {
+  if (id) {
+    const vehicle = window.dataStore.cache.searchData[id];
+    document.querySelector('.modal').classList.add('modal--active');
+    document.querySelector('.modal').innerHTML = `     <div class="modal-body">
+        <div>
+          <button onclick="document.querySelector('.modal').classList.remove('modal--active');"></button>
+        </div>
+        <div class="modal-wrap">
+          <div class="vehicle-item">
+            <span 
+              class="vehicle-type ${
+                vehicle.is_premium ? `type-${vehicle.type}--premium` : `type-${vehicle.type}`
+              }"
+            >
+              ${getRomeNumber(vehicle.tier)}
+            </span>
+            <span 
+              class="vehicle-flag vehicle-flag-${vehicle.nation}"
+            ></span>
+            <img 
+              class="vehicle-img"
+              src="${vehicle.images.big_icon}"
+              alt="${vehicle.short_name}"
+            > 
+          </div>
+          <div>
+            <p class="vehicle-prop">
+              <b class="vehicle-name">${vehicle.name}</b> - 
+              <b class="vehicle-price">${vehicle.price_credit}</b>
+            </p>
+            <p class="vehicle-prop vehicle-description">${vehicle.description}</p>
+          </div>
+        </div>
+        <p class="vehicle-prop">
+          <span class="vehicle-hp_title">Прочность: </span><b class="vehicle-hp">${
+            vehicle.default_profile.hp
+          }</b>
+        </p>
+        <p class="vehicle-prop">
+          <span class="vehicle-max_ammo_title">Боекомплект: </span><b class="vehicle-max_ammo">${
+            vehicle.default_profile.max_ammo
+          }</b>
+        </p>
+        <p class="vehicle-prop">
+          <span class="vehicle-_title"> :</span><b class="vehicle-">${
+            vehicle.default_profile.max_ammo
+          }</b>
+        </p>
+        <p class="vehicle-prop">
+          <span class="vehicle-speed_forward_title">Максимальная скорость (км/ч) :</span><b class="vehicle-speed_forward">${
+            vehicle.default_profile.speed_forward
+          }</b>
+        </p>
+        <p class="vehicle-prop">
+          <span class="vehicle-speed_backward_title">Макс. скорость заднего хода (км/ч) :</span><b class="vehicle-speed_backward">${
+            vehicle.default_profile.speed_backward
+          }</b>
+        </p>
+        <p class="vehicle-prop">
+          <span class="vehicle-weight_title">Масса (кг):</span><b class="vehicle-weight">${
+            vehicle.default_profile.weight
+          }</b>
+        </p>
       </div>
     `;
   } else {
@@ -100,26 +151,26 @@ function getVehicleList() {
       .map(vehicle => {
         return `
           <div 
-            class="vehicle-item" 
-            data-type='vehicleData'
-            data-value='tank_id-${vehicle.tank_id}' 
-            onclick='(${searchById})(this.dataset)'
+            class="vehicle-item"
+            data-type="vehicleData"
+            data-id="${vehicle.tank_id}"
+            onclick="(window.searchById)(this.dataset.id)"
           > 
             <span 
               class="vehicle-type ${
                 vehicle.is_premium ? `type-${vehicle.type}--premium` : `type-${vehicle.type}`
               }"
             >
-              ${vehicle.tier}
+              ${getRomeNumber(vehicle.tier)}
             </span>
             <span 
               class="vehicle-flag vehicle-flag-${vehicle.nation}"
             ></span>
             <img 
-              class="vehicle-img" 
-              src="${vehicle.images.big_icon}" 
+              class="vehicle-img"
+              src="${vehicle.images.big_icon}"
               alt="${vehicle.short_name}"
-            > 
+            /> 
             <span 
               class="vehicle-title ${vehicle.is_premium ? 'vehicle-title--premium' : ''}"
             >
@@ -129,78 +180,87 @@ function getVehicleList() {
       })
       .join('');
   } else {
-    return '';
+    return ``;
   }
 }
 
 function getVehicleTypes() {
-  const { vehicle_types: vehicleTypes } = window.dataStore.cache.generalData;
+  if (window.dataStore.cache.generalData) {
+    const { vehicle_types: vehicleTypes } = window.dataStore.cache.generalData;
 
-  return `
-    <ul class='nation-list'>
-        ${Object.keys(vehicleTypes)
-          .map(type => {
-            return `
-              <li 
-                class='type-item' 
-                data-type='searchData'
-                data-value='type-${type}'
-                onclick='(${window.searchByFilter})(this.dataset);'
-              >
-                <img 
-                  class='type-img' 
-                  src='./images/types/${type.toLocaleLowerCase()}.png' 
-                  alt='${vehicleTypes[type]}'>
-                <span>
-                  ${vehicleTypes[type]}
-                </span>
-              </li>`;
-          })
-          .join('')}
-    </ul>`;
+    return `
+      <ul class="nation-list">
+          ${Object.keys(vehicleTypes)
+            .map(type => {
+              return `
+                <li 
+                  class="type-item"
+                  data-type="searchData"
+                  data-value="type_${type}"
+                  onclick="(window.searchByFilter)(this);"
+                >
+                  <img 
+                    class="type-img"
+                    src="./images/types/${type.toLocaleLowerCase()}.png"
+                    alt="${vehicleTypes[type]}">
+                  <span>
+                    ${vehicleTypes[type]}
+                  </span>
+                </li>`;
+            })
+            .join('')}
+      </ul>`;
+  } else {
+    return '';
+  }
 }
 
 function getVehicleNations() {
-  const { vehicle_nations: nationList } = window.dataStore.cache.generalData;
+  if (window.dataStore.cache.generalData) {
+    const { vehicle_nations: nationList } = window.dataStore.cache.generalData;
 
-  return `
-    <ul class='nation-list'>
-        ${Object.keys(nationList)
-          .map(nation => {
-            return `
-              <li 
-                class='nation-item' 
-                data-type='searchData'
-                data-value='nation-${nation}'
-                onclick='(${window.searchByFilter})(this.dataset);'
-              >
-                <img 
-                  class='nation-img' 
-                  src='./images/flags/${nation}.png' 
-                  alt='${nationList[nation]}'
+    return `
+      <ul class="nation-list">
+          ${Object.keys(nationList)
+            .map(nation => {
+              return `
+                <li 
+                  class="nation-item"
+                  data-type="searchData"
+                  data-value="nation_${nation}"
+                  onclick="(window.searchByFilter)(this);"
                 >
-                <span>
-                  ${nationList[nation]}
-                </span>
-              </li>`;
-          })
-          .join('')}
-    </ul>`;
+                  <img 
+                    class="nation-img"
+                    src="./images/flags/${nation}.png"
+                    alt="${nationList[nation]}"
+                  >
+                  <span>
+                    ${nationList[nation]}
+                  </span>
+                </li>`;
+            })
+            .join('')}
+      </ul>`;
+  } else {
+    return '';
+  }
 }
 
 function App() {
   return `
     <div class="nation-wrap">
+        <h2 class="nation-title">Нация</h2>
         ${getVehicleNations()}
     </div>
     <div class="type-wrap">
+        <h2 class="type-title">Вид</h2>
         ${getVehicleTypes()}
     </div>
     <div class="vehicle-wrap">
         ${getVehicleList()}
     </div>
-    <div class="infoVehicle-wrap">
-        ${getVehicle()}
+    <div class="infoVehicle-wrap modal ">
     </div>
-  `;
+    `;
 }
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,6 @@
 const parameters = {
-  application_id: process.env.WOT_API_ID_KEY,
+  //   application_id: process.env.WOT_API_ID_KEY,
+  application_id: '42a820be7f4b3fa53490a3eebeae0521',
 };
 
 const getStringParams = params => {
@@ -10,8 +11,29 @@ const getStringParams = params => {
     .join('');
 };
 
+const romeNumber = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+};
+
+export const filterByCategory = (list, category) => {};
+
+export const filterByTier = list => {};
+
 export const getUrl = (path, param = {}) => {
   let params = { ...parameters, ...param };
   getStringParams(params);
   return `https://api.worldoftanks.ru/wot/encyclopedia/${path}/?${getStringParams(params)}`;
+};
+
+export const getRomeNumber = num => {
+  return romeNumber[num];
 };
